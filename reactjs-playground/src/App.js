b236dedb6df7fecbe2de7386de171bb2ffd4db40
Sebastian Terrazas <terrazas.sebastian@hotmail.com>
2020-06-05 18:06:03 -0600

    react begining + ex20

---
+++ reactjs-playground/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Descripcion from './components/Descripcion'
+import Evento from './components/Evento';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my playground! Wanna play?</h1> {/* excuse the mess */}
+      <Descripcion />
+      <Evento />
+    </div>
+  );
+}
+
+export default App;
